dc06eb8bd68eb163e6b923bae85fd2a441a245c5
On Khang <ondinhk@gmail.com>
2022-04-04 17:46:59 +0700

    Delete lib not use in Dalat model

--- src/main/java/com/ondinhk/nienluan/model/Dalat.java
+++ src/main/java/com/ondinhk/nienluan/model/Dalat.java
@@ -4,8 +4,6 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 
 @Document(collection = "info_dalat")
 public class Dalat {
